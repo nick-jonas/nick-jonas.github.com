fa55f99d47aa1a21b8e15b49aab8a929e9e24dc4
Nick Jonas <njonas45@gmail.com>
2013-02-18 00:49:45 -0500

    demo update

--- threesixtyjs/assets/js/jquery.threesixty.js
+++ threesixtyjs/assets/js/jquery.threesixty.js
@@ -1,6 +1,6 @@
 /*!
  * ThreeSixty: A jQuery plugin for generating a draggable 360 preview from an image sequence.
- * Version: 0.1.1
+ * Version: 0.1.2
  * Original author: @nick-jonas
  * Website: http://www.workofjonas.com
  * Licensed under the MIT license
@@ -112,7 +112,7 @@ var scope,
             var $this = $(this),
                 path = $this.data('path'),
                 count = $this.data('count');
-            data.push({'path': path, 'count': count});
+            data.push({'path': path, 'count': count, 'loaded': 0, '$el': $this});
             total += count;
         });
 
@@ -132,39 +132,39 @@ var scope,
             j = 0;
             for(j; j < data[i].count; j++){
                 url = data[i].path.replace('{index}', j);
-                $('<img/>').attr('src', url).load(this.onLoadComplete);
+                $('<img/>').data('index', i).attr('src', url).load(this.onLoadComplete);
             }
         }
     };
 
-    ThreeSixty.prototype.onLoadComplete = function() {
-        loaded++;
-        if(loaded === total){
-            scope.onLoadAllComplete();
+    ThreeSixty.prototype.onLoadComplete = function(e) {
+        var index = $(e.currentTarget).data('index'),
+            thisObj = data[index];
+        thisObj.loaded++;
+        if(thisObj.loaded === thisObj.count){
+            scope.onLoadAllComplete(index);
         }
     };
 
-    ThreeSixty.prototype.onLoadAllComplete = function() {
-        $el.each(function(index){
-            var $this = $(this),
-                html = '',
-                l = data[index].count,
-                pathTemplate = data[index].path,
-                i = 0;
+    ThreeSixty.prototype.onLoadAllComplete = function(objIndex) {
+        var $this = data[objIndex].$el,
+            html = '',
+            l = data[objIndex].count,
+            pathTemplate = data[objIndex].path,
+            i = 0;
 
-            // remove preloader
-            $this.html('');
-            $this.removeClass('preloading');
+        // remove preloader
+        $this.html('');
+        $this.removeClass('preloading');
 
-            // add 360 images
-            for(i; i < l; i++){
-                var display = (i === 0) ? 'block' : 'none';
-                html += '<img class="threesixty-frame" style="display:' + display + ';" data-index="' + i + '" src="' + pathTemplate.replace('{index}', i) + '"/>';
-            }
-            $this.html(html);
-        });
+        // add 360 images
+        for(i; i < l; i++){
+            var display = (i === 0) ? 'block' : 'none';
+            html += '<img class="threesixty-frame" style="display:' + display + ';" data-index="' + i + '" src="' + pathTemplate.replace('{index}', i) + '"/>';
+        }
+        $this.html(html);
 
-        this.attachHandlers();
+        this.attachHandlers(objIndex);
     };
 
     var startY = 0,
@@ -174,49 +174,48 @@ var scope,
         lastX = 0,
         lastVal = 0,
         isMouseDown = false;
-    ThreeSixty.prototype.attachHandlers = function() {
+    ThreeSixty.prototype.attachHandlers = function(objIndex) {
         var that = this;
-        $el.each(function(i){
-            var $this =$(this);
+        var $this = data[objIndex].$el;
 
-            // add draggable events
-            if(options.draggable){
-                // if touch events supported, use
-                if(typeof document.ontouchstart !== 'undefined' &&
-                    typeof document.ontouchmove !== 'undefined' &&
-                    typeof document.ontouchend !== 'undefined' &&
-                    typeof document.ontouchcancel !== 'undefined'){
-                    var elem = $this.get()[0];
-                    elem.addEventListener('touchstart', that.onTouchStart);
-                    elem.addEventListener('touchmove', that.onTouchMove);
-                    elem.addEventListener('touchend', that.onTouchEnd);
-                    elem.addEventListener('touchcancel', that.onTouchEnd);
-                }
+        // add draggable events
+        if(options.draggable){
+            // if touch events supported, use
+            if(typeof document.ontouchstart !== 'undefined' &&
+                typeof document.ontouchmove !== 'undefined' &&
+                typeof document.ontouchend !== 'undefined' &&
+                typeof document.ontouchcancel !== 'undefined'){
+                var elem = $this.get()[0];
+                elem.addEventListener('touchstart', that.onTouchStart);
+                elem.addEventListener('touchmove', that.onTouchMove);
+                elem.addEventListener('touchend', that.onTouchEnd);
+                elem.addEventListener('touchcancel', that.onTouchEnd);
             }
+        }
 
-            // mouse down
-            $this.mousedown(function(e){
-                e.preventDefault();
-                thisTotal = $(this).data('count');
-                $downElem = $(this);
-                startY = e.screenY;
-                lastVal = $downElem.data('lastVal') || 0;
-                lastX = $downElem.data('lastX') || 0;
-                lastY = $downElem.data('lastY') || 0;
-                isMouseDown = true;
-            });
+        // mouse down
+        $this.mousedown(function(e){
+            e.preventDefault();
+            thisTotal = $(this).data('count');
+            $downElem = $(this);
+            startY = e.screenY;
+            lastVal = $downElem.data('lastVal') || 0;
+            lastX = $downElem.data('lastX') || 0;
+            lastY = $downElem.data('lastY') || 0;
+            isMouseDown = true;
+            $downElem.trigger('down');
+        });
 
-            // arrow keys
-            if(options.useKeys === true){
-                $(document).bind('keydown', that.onKeyDown);
-            }
+        // arrow keys
+        if(options.useKeys === true){
+            $(document).bind('keydown', that.onKeyDown);
+        }
 
-            // mouse up
-            $(document, 'html', 'body').mouseup(that.onMouseUp);
-            $(document).blur(that.onMouseUp);
-            $('body').mousemove(function(e){
-                that.onMove(e.screenX, e.screenY);
-            });
+        // mouse up
+        $(document, 'html', 'body').mouseup(that.onMouseUp);
+        $(document).blur(that.onMouseUp);
+        $('body').mousemove(function(e){
+            that.onMove(e.screenX, e.screenY);
         });
     };
 
@@ -231,6 +230,7 @@ var scope,
         lastX = $downElem.data('lastX') || 0;
         lastY = $downElem.data('lastY') || 0;
         isMouseDown = true;
+        $downElem.trigger('down');
     };
 
     ThreeSixty.prototype.onTouchMove = function(e) {
@@ -249,6 +249,8 @@ var scope,
                 y = screenY,
                 val = 0;
 
+            $downElem.trigger('move');
+
             if(options.dragDirection === 'vertical'){
                 if(y > lastY){
                     val = lastVal + 1;
@@ -258,6 +260,8 @@ var scope,
             }else{
                 if(x > lastX){
                     val = lastVal + 1;
+                }else if(x === lastX){
+                    return;
                 }else{
                     val = lastVal - 1;
                 }
@@ -295,6 +299,7 @@ var scope,
 
     ThreeSixty.prototype.onMouseUp = function(e) {
         isMouseDown = false;
+        $downElem.trigger('up');
     };
 
     /**
