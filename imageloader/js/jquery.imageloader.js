a0b81ab03d2c799be41bb47b9a819dfc6c6f090d
Nick Jonas <njonas45@gmail.com>
2013-06-24 17:22:38 -0400

    removed SS webfonts everywhere

--- imageloader/js/jquery.imageloader.js
+++ imageloader/js/jquery.imageloader.js
@@ -1,92 +1,42 @@
 /*!
  * imageloader : a jquery plugin for preloading images
- * Version: 0.1.0
+ * Version: 0.2.0
  * Original author: @nick-jonas
  * Website: http://www.workofjonas.com
  * Licensed under the MIT license
  */
 
-;(function ( $, window, document, undefined ) {
-
-    var options = {
-            urls: [],
-            onComplete: function(images){},
-            onUpdate: function(image, ratio){},
-            onError: function(err){}
-        },
-        loadCount = 0,
+;(function ($) {
+ 
+  $.imageloader = function(userOptions) {
+ 
+    var options = $.extend({
+      urls: [],
+      onComplete: function() {},
+      onUpdate: function(ratio, image) {},
+      onError: function(err) {}
+    }, userOptions);
+    
+    var loadCount = 0,
         completedUrls = [],
-        len = 0;
+        urls = options.urls,
+        len = urls.length;
 
-        /**
-         * load all images
-         * @return null
-         */
-        _initLoad = function(){
-            var i = 0;
-            for(i; i < len; i++){
-                _loadImage(i);
-            }
-        },
+    $.each(urls, function(i, item) {
+      var img = new Image(), error = false;
+      img.src = item;
+      img.onerror = function() {
+        loadCount++;
+        options.onError('Error loading image: ' + item);
+      };
+      $('<img/>').attr('src', item).load(function(res) {
+        loadCount++;
+        options.onUpdate(loadCount/len, urls[loadCount-1]);
+        if (loadCount === len) options.onComplete();
+      });
+    });
 
-        _getRatio = function(){
-            return loadCount / len;
-        },
+  };
+ 
+})(jQuery);
 
-        /**
-         * load single image
-         * @param  {uint} i index of image from array of urls
-         * @return null
-         */
-        _loadImage = function(i){
-            var img = new Image(),
-                error = false,
-                msg = 'Sorry, but there was an error loading: ';
-            img.src = options.urls[i];
-            img.onerror = function(){
-                loadCount++;
-                _onLoadError(msg + options.urls[i]);
-            };
-
-            $('<img/>').attr('src', options.urls[i]).load(function(response){
-                loadCount++;
-                completedUrls.push(options.urls[i]);
-                _onImageLoadComplete(options.urls[i]);
-                if(loadCount === len){
-                    _onLoadAllComplete();
-                }
-            });
-        },
-
-        /**
-         * image load handler
-         * @return null
-         */
-        _onImageLoadComplete = function(image){
-            var ratio = _getRatio();
-            options.onUpdate(_getRatio(), image);
-        },
-
-        /**
-         * handler for when all images have been loaded
-         * @return null
-         */
-        _onLoadAllComplete = function(){
-            var ratio = _getRatio();
-            if(ratio >= 1){
-                options.onComplete(completedUrls);
-            }
-        },
-
-        _onLoadError = function(err){
-            options.onError(err);
-        };
-
-
-    $.imageloader = function(userOptions){
-        options = $.extend({}, options, userOptions);
-        len = options.urls.length;
-        _initLoad();
-    };
-
-})( jQuery, window, document );
--- windows/README.md
+++ windows/README.md
@@ -2,19 +2,61 @@
 
 ---
 
-#### plugin description
+#### a handy, loosely-coupled jQuery plugin for full-screen scrolling windows
 
 ## Usage
 
 Include the latest jQuery and `assets/js/jquery.windows.js` in your HTML page.
+
+#### html
+
+```html
+<section class="window"></section>
+<section class="window"></section>
+<section class="window"></section>
+<section class="window"></section>
+<section class="window"></section>
+```
 
 #### js
 
 ```javascript
 $(document).ready(function(){
 
+    $('.window').windows({
+        snapping: true,
+        snapSpeed: 500,
+        snapInterval: 1100,
+        onScroll: function(scrollPos){
+            // scrollPos:Number
+        },
+        onSnapComplete: function($el){
+            // after window ($el) snaps into place
+        },
+        onWindowEnter: function($el){
+            // when new window ($el) enters viewport
+        }
+    })
 
 });
+```
+
+#### scss
+
+```scss
+.window{
+    width:100%;
+    height:100%;
+    position:absolute;
+}
+
+@for $i from 1 through 6{
+    .window:nth-child(#{$i}){
+        background:nth($colors, $i);
+        top:($i - 1) * 100%;
+    }
+}
+
 ```
 
 ## In the Wild
